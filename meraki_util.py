235de9bdc11c2963113774337af36f898fe258c8
Jaedan Calder <jcalder@hammond.com.au>
2024-08-14 13:48:47 +1000

    Added function

--- meraki_util.py
+++ meraki_util.py
@@ -431,6 +431,84 @@ def get_device_appliance_uplinks_settings(
     return dashboard.appliance.getDeviceApplianceUplinksSettings(serial)
 
 
+# @cache_json("cache/getDeviceSwitchPortsStatuses.json")
+def get_device_switch_ports_statuses(dashboard: meraki.DashboardAPI, serial: str):
+    """https://developer.cisco.com/meraki/api-v1/get-device-switch-ports-statuses/
+    ```json
+    [
+        {
+            "portId": "1",
+            "enabled": true,
+            "status": "Connected",
+            "spanningTree": {
+                "statuses": [ "Learning" ]
+            },
+            "isUplink": false,
+            "errors": [
+                "PoE overload",
+                "Very high proportion of CRC errors"
+            ],
+            "warnings": [
+                "SecurePort authentication in progress",
+                "PoE port was denied power",
+                "High proportion of CRC errors"
+            ],
+            "speed": "10 Gbps",
+            "duplex": "full",
+            "usageInKb": {
+                "total": 40867,
+                "sent": 23008,
+                "recv": 17859
+            },
+            "cdp": {
+                "systemName": "",
+                "platform": "MS350-24X",
+                "deviceId": "0c8ddbddee:ff",
+                "portId": "Port 20",
+                "nativeVlan": 1,
+                "address": "10.0,0.1",
+                "managementAddress": "10.0.0.100",
+                "version": "1",
+                "vtpManagementDomain": "",
+                "capabilities": "Switch"
+            },
+            "lldp": {
+                "systemName": "MS350-24X - Test",
+                "systemDescription": "MS350-24X Cloud Managed PoE Switch",
+                "chassisId": "0c:8d:db:dd:ee:ff",
+                "portId": "20",
+                "managementVlan": 1,
+                "portVlan": 1,
+                "managementAddress": "10.0.0.100",
+                "portDescription": "Port 20",
+                "systemCapabilities": "switch"
+            },
+            "clientCount": 10,
+            "powerUsageInWh": 55.9,
+            "trafficInKbps": {
+                "total": 2.2,
+                "sent": 1.2,
+                "recv": 1
+            },
+            "securePort": {
+                "enabled": true,
+                "active": true,
+                "authenticationStatus": "Authentication in progress",
+                "configOverrides": {
+                    "type": "trunk",
+                    "vlan": 12,
+                    "voiceVlan": 34,
+                    "allowedVlans": "all"
+                }
+            },
+            "poe": { "isAllocated": false }
+        }
+    ]
+    ```
+    """
+    return dashboard.switch.getDeviceSwitchPortsStatuses(serial)
+
+
 # @cache_json("cache/getNetworkAppliancePorts,json", verbose=False)
 def get_network_appliance_ports(
     dashboard: meraki.DashboardAPI, network_id: str):
